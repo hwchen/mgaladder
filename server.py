3b8e146ae8bc5e68b94a7543de8ad26c1ba9aee2
Walther Chen <walther.chen@gmail.com>
2015-01-27 22:35:18 -0500

    Initial Work for DynamoDB
    - configure environmental vars for dynamoDB
    - configure and import pynamoDB
    - create test class DB_players

--- server.py
+++ server.py
@@ -2,6 +2,13 @@
 
 # Flask server for MGA ladder
 
+import os
+
+from pynamodb.models import Model
+from pynamodb.attributes import (
+    UnicodeAttribute, NumberAttribute
+)
+
 from flask import Flask, jsonify
 
 app = Flask(__name__, static_url_path='')
@@ -19,5 +26,29 @@ def results():
         ]}
     return jsonify(resultsList)
 
+@app.route('/rankings')
+def rankings():
+    rankingsList = {"rankings": [
+        {"Walther": "5d"},
+        {"Andrew": "1k"}
+        ]}
+    return jsonify(rankingsList)
+
+# Database
+
+class DB_players (Model):
+    class Meta:
+        table_name = 'players'
+        host = 'http://localhost:8000'
+    player_id = NumberAttribute(hash_key=True)
+    player_name = UnicodeAttribute(range_key=True)
+
+def test():
+    print('hello world')
+
 if __name__ == '__main__':
+    if not DB_players.exists():
+        DB_players.create_table(read_capacity_units=1,
+                                write_capacity_units=1,
+                                wait=True)
     app.run(debug=True)
